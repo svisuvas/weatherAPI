de36ebd51124e495a0745c88af44effd005b6f60
sansuns360 <sansuns360@gmail.com>
2019-08-02 11:05:49 -0400

    changes for local run

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var http = require('http'),
     fs = require('fs'),
     httpProxy = require('http-proxy'),
-    request = require("request-promise") ;
+	request = require("request-promise") ;
+	
+const HTTPPORT = 8000;
+const PROXYPORT = 9000;
+const HOST = '0.0.0.0';
 
-var apiProxy = httpProxy.createProxyServer({target:'http://localhost:9000'}).listen(8000);
+var apiProxy = httpProxy.createProxyServer({target:'http://0.0.0.0:9000'}).listen(8000);
 
-var dataProp = {city:"", state: "", cwa: "", zip:"", geoLat: "", geoLon: "", gridX:"", gridY:"", elevation:"", forecastUri:""};
+var dataProp = {city:"", state: "", cwa: "", geoLat: "", geoLon: "", gridX:"", gridY:"", elevation:"", forecastUri:""};
 
 var options = {
 	  uri:  '',
@@ -17,7 +21,7 @@ var options = {
 	  cert: fs.readFileSync('./myfile.crt'),
 	  json: true,
 	  headers: {
-		  'token': 'ENTER_YOUR_TOKEN',
+		  'token': 'DegDwzrxbuaBcvRnxByISaKTuILimTHg',
 		  'User-Agent': 'request',
 		  'accept' : 'application/json'
 	  }
@@ -48,10 +52,12 @@ var weather = {
 		var gridY = forecastProps.properties.gridY;
 
 		var elevation = 0;
-
-
+ 
 		var geoLat = forecastProps.geometry.coordinates[1];
 		var geoLon = forecastProps.geometry.coordinates[0];
+
+
+
 
 		dataProp.city = city;
 		dataProp.state = state;
@@ -62,6 +68,7 @@ var weather = {
 		dataProp.forecastUri = forecastUri;
 		dataProp.geoLat = geoLat;
 		dataProp.geoLon = geoLon;
+
 
 		options.uri = forecastUri;
 		options.path = 'gridpoints/'.concat(cwa).concat('/').concat(gridX).concat(',').concat(gridY).concat('/forecast');
@@ -92,14 +99,33 @@ http.createServer(function (req, res) {
 		 
 		 main( geoPoints ).then(function(result){
 
+		 	//result.dataProp = dataProp;
+		 	//result.dataProp.elevation = result.properties.elevation.value;
+		 	dataProp.elevation = result.properties.elevation.value;
+
+			var currentTemperature = result.properties.periods[0].temperature;
+			var currentWindSpeed = result.properties.periods[0].windSpeed;
+			var currentWindDirection = result.properties.periods[0].windDirection;
+			var shortForecast = result.properties.periods[0].shortForecast;
+
+			dataProp.currentTemperature = currentTemperature;
+			dataProp.currentWindSpeed = currentWindSpeed;
+			dataProp.currentWindDirection = currentWindDirection;
+			dataProp.shortForecast = shortForecast;
+
+
 		 	result.dataProp = dataProp;
-		 	result.dataProp.elevation = result.properties.elevation.value;
 
 		 	console.log(JSON.parse(JSON.stringify(result)));
 
-		   res.writeHead(200, { 'Content-Type': 'text/plain' });
-	       res.write('request successfully proxied! for '  + '\n' + JSON.stringify(result, true, 2));
-	       res.end();
+		   //res.writeHead(200, { 'Content-Type': 'text/plain' }); application/json
+		   res.writeHead(200, { 'Content-Type': 'application/json' });
+
+	       ///res.write('request successfully proxied! for '  + '\n' + JSON.stringify(dataProp, true, 2));
+	      // res.write(JSON.stringify(dataProp, true, 2));
+	      var json = JSON.stringify(dataProp, true, 2);
+	        
+	       res.end(json);
 
 		 });
 
